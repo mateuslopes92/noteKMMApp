648a8d6d8af29e83aae2ad240278d67094bc9fc4
mateuslopes92 <mateuslopes92@gmai.com>
2023-08-23 10:19:59 -0300

    android code

--- shared/src/commonMain/kotlin/com/example/notekmmapp/data/note/SqlDelightNoteDataSource.kt
+++ shared/src/commonMain/kotlin/com/example/notekmmapp/data/note/SqlDelightNoteDataSource.kt
@@ -6,22 +6,31 @@ import com.example.notekmmapp.domain.note.NoteDataSource
 import com.example.notekmmapp.domain.time.DateTimeUtil
 
 class SqlDelightNoteDataSource(db: NoteDatabase): NoteDataSource {
+
     private val queries = db.noteQueries
 
     override suspend fun insertNote(note: Note) {
-        queries.insertnote(
+        queries.insertNote(
             id = note.id,
             title = note.title,
             content = note.content,
-            colorHex = note.colorHex, created = DateTimeUtil.toEpochMillis(note.created))
+            colorHex = note.colorHex,
+            created = DateTimeUtil.toEpochMillis(note.created)
+        )
     }
 
     override suspend fun getNoteById(id: Long): Note? {
-        return queries.getNoteById(id).executeAsOneOrNull()?.toNote()
+        return queries
+            .getNoteById(id)
+            .executeAsOneOrNull()
+            ?.toNote()
     }
 
     override suspend fun getAllNotes(): List<Note> {
-        return queries.getAllNotes().executeAsList().map {it.toNote()}
+        return queries
+            .getAllNotes()
+            .executeAsList()
+            .map { it.toNote() }
     }
 
     override suspend fun deleteNoteById(id: Long) {
