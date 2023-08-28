130e2336fb7eb57c2909f8766c75404d0615ab7c
Mateus Lopes <mateuslopes92@gmail.com>
2023-08-27 22:39:30 -0300

    note detail screen

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -53,8 +53,9 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
 
-    implementation("com.google.dagger:hilt-android:2.42")
-    kapt("com.google.dagger:hilt-android-compiler:2.42")
+    implementation("com.google.dagger:hilt-android:2.47")
+    kapt("com.google.dagger:hilt-android-compiler:2.47")
     kapt("androidx.hilt:hilt-compiler:1.0.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation("androidx.navigation:navigation-compose:2.5.3")
 }
